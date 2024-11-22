5660c1b98e978363162a37a6867e856a1600f9d8
eithan <eithan.nakache@epita.fr>
2024-11-22 16:56:53 +0100

    feat: add norm in patch embedding

--- src/layers.py
+++ src/layers.py
@@ -4,12 +4,16 @@ from torch import nn
 class PatchEmbeddings(nn.Module):
     def __init__(self, in_channels=1, patch_size=8, embedding_dim=128):
         super().__init__()
-        self.projection = nn.Conv2d(in_channels, embedding_dim, kernel_size=patch_size, stride=patch_size)
+        self.projection = nn.Conv2d(
+            in_channels, embedding_dim, kernel_size=patch_size, stride=patch_size
+        )
+        self.norm = nn.LayerNorm(embedding_dim)
 
     def forward(self, x):
         x = self.projection(x)
         B, C, H, W = x.shape
-        x = x.view(B,C,H*W).transpose(1,2)
+        x = x.view(B, C, H * W).transpose(1, 2)
+        x = self.norm(x)
         return x
 
 
@@ -27,6 +31,7 @@ class MLP(nn.Module):
         self.fc2 = nn.Linear(in_features=hidden_features, out_features=out_features)
         self.dropout = nn.Dropout(p=dropout)
         self.activation = activation()
+        self.norm = nn.LayerNorm(hidden_features)
 
     def forward(self, x):
         x = self.fc1(x)
